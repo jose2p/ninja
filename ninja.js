c783694d203cfd26364a7cfe467911326a91277c
jose2p <alvaromorel.134@gmail.com>
2023-01-10 19:48:40 -0300

    "[TAREA] sensei"

--- ninja.js
+++ ninja.js
@@ -15,8 +15,23 @@ class Ninja {
         this.salud += 10;
     }
 }
-const ninja1 = new Ninja("Alvaro", 5);
-ninja1.sayName();
-ninja1.drinkSnake();
-ninja1.showStats();
+class Sensei extends Ninja{
+    constructor(nombre){
+        super(nombre,200);
+        this.nombre = nombre;
+        this.velocidad = 10;
+        this.fuerza = 10;
+        this.sabiduría = 10;
+    }
+    speakWisdom(){
+        this.drinkSnake();
+        let msg = "Lo que un programador puede hacer en un mes, dos programadores pueden hacerlo en dos meses";
+        console.log(msg);
+    }
+}
+const superSensei = new Sensei("Master Splinter");
+superSensei.speakWisdom();
+// -> "Lo que un programador puede hacer en un mes, dos programadores pueden hacerlo en dos meses."
+superSensei.showStats();
+// -> "Nombre: Master Splinter, Salud: 210, Velocidad: 10, Fuerza: 10"
 
